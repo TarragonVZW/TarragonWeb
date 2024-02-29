540bd63d58002ad6e1f8f38caa1e904316d00bb9
Jasper Goens <jasper_goens@hotmail.com>
2024-02-29 12:31:29 +0100

    update desktop header

--- src/modules/Navbar.js
+++ src/modules/Navbar.js
@@ -9,7 +9,12 @@ function Navbar() {
                 <img className="tinylogo" src={Tarragon_Tiny} alt="Tarragon Tiny Logo"/>
                 <img className="title" src={Tarragon_Title} alt="Tarragon Title Logo"/>
             </Link>
-
+            <div className={"headerlinks"}>
+                <a href="/#Home"><h2>Home</h2></a>
+                <a href="/#Kobolds"><h2>Kobolds</h2></a>
+                <a href="/LNOE"><h2>Last Night on Earth</h2></a>
+                <a href="https://discord.com/invite/TjDUu2Gkag" target="_blank"><h2>Discord</h2></a>
+            </div>
         </header>
     )
 }
