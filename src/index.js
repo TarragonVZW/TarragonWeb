5c697e18539049122f73fa8e0f816d6b5cce4ab0
Jasper Goens <jasper_goens@hotmail.com>
2024-03-05 22:13:28 +0100

    switch to hashrouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import './index.css';
 import App from './App';
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 import { createRoot } from 'react-dom/client';
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 root.render(<React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
         < App/>
-    </BrowserRouter>
+    </HashRouter>
 </React.StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
--- src/modules/Navbar.js
+++ src/modules/Navbar.js
@@ -5,14 +5,14 @@ import {Link} from "react-router-dom";
 function Navbar() {
     return (
         <header>
-            <a href="/">
+            <a href="/#/">
                 <img className="tinylogo" src={Tarragon_Tiny} alt="Tarragon Tiny Logo"/>
                 <img className="title" src={Tarragon_Title} alt="Tarragon Title Logo"/>
             </a>
             <div className={"headerlinks"}>
-                <a href="/#Home"><h2>Home</h2></a>
-                <a href="/#Kobolds"><h2>Kobolds</h2></a>
-                <a href="/LNOE"><h2>Last Night on Earth</h2></a>
+                <a href="/#/#Home"><h2>Home</h2></a>
+                <a href="/#/#Kobolds"><h2>Kobolds</h2></a>
+                <a href="/#/LNOE"><h2>Last Night on Earth</h2></a>
                 <a href="https://discord.com/invite/TjDUu2Gkag" target="_blank" rel="noreferrer"><h2>Discord</h2></a>
             </div>
         </header>
