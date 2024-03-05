39e6770b46e46d60fd8fe73cf3fa1b93e90dc450
Jasper Goens <jasper_goens@hotmail.com>
2024-03-05 23:21:00 +0100

    fuck around

--- tina/config.ts
+++ tina/config.ts
@@ -49,7 +49,37 @@ export default defineConfig({
         ],
         ui: {
           // This is an DEMO router. You can remove this to fit your site
-          router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+            router: ({ document }) => `/demo/blog/${document._sys.filename}`,
+        },
+      },
+      {
+        name: "event",
+        label: "Events",
+        path: "content/events",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+        ],
+        ui: {
+          // This is an DEMO router. You can remove this to fit your site
+          router: ({ document }) => `/live/event/${document._sys.filename}`,
         },
       },
     ],
